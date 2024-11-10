7b6c330cddf51bbed7c9f1b41fc6b40b3b53d65d
larinharls <lara.raquelfo@gmail.com>
2024-11-09 22:07:00 -0300

    continuando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,11 +12,37 @@ const Header = () => {
             <div className="search-bar">
                 <input type="text" className="search-input" placeholder="Pesquisar produto...">
                 </input>
-                <button className="search-btn"></button>
             </div>
             <div className="login">
                 <a href="#" className="link-cadastro">Cadastre-se</a>
-                <a href="#" className="btn-entrar">Entrar</a>
+                <button className="btn-entrar">Entrar</button>
+            </div>
+            <div className="icon-carrinho">
+                <img className="carrinho" src="/carrinho.png" alt="icone de carrinho" />
+            </div>
+            <div className="menu">
+                <ul className="menu-nav">
+                    <li><a className="link" href="">Home</a></li>
+                    <li><a className="link" href="">Produtos</a></li>
+                    <li><a className="link" href="">Categorias</a></li>
+                    <li><a className="link" href="">Meus pedidos</a></li>
+                </ul>   
+            </div>
+
+            <div className="carousel">
+                <div className="text-carousel">
+                    <p>Melhores ofertas personalizadas</p>
+                    <h1>Queima de estoque Nike <img src="/fire.jpg"/></h1>
+                </div>
+            </div>
+            <div className="description-carousel">
+                <p>Consequat culpa exercitation mollit nisi excepteur do tempor laboris eiusmod irure consectetur.</p>
+            </div>
+            <div className="btn-carousel">
+                <button className="btn-offer">Ver ofertas</button>
+            </div>
+            <div className="tenis">
+                <img src="/tenis-nike.png" />
             </div>
         </header>
     )
