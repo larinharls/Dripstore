da9be02b3f46ce0f7255b0abd69b7804423dfb01
larinharls <lara.raquelfo@gmail.com>
2024-11-16 18:22:11 -0300

    atualizando header

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,8 +4,12 @@ import './Header.css'
 const BoxSlide = () => {
     return (
 
-        <div className="carousel">
+        <div className="container">
             <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <h1 class="text-start mt-5 mb-4 fs-6 text-warning">Melhores ofertas personalizadas</h1>
+                    <p className="fs-1 fw-bold mb-4">Queima de<br></br> estoque Nike</p>
+                        <h1 className="fs-6">Consequat culpa exercitation mollit nisi excepteur do<br></br> do tempor laboris eiusmod irure consectetur.</h1>
+                        <button type="button" class="btn btn-primary btn-lg">Ver ofertas</button>
                 <div class="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -32,6 +36,8 @@ const BoxSlide = () => {
                 </button>
             </div>
         </div>
+        
+        
 
 
 
