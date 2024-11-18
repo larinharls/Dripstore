1e3922ca323cb157ab25f473a2757c2cbf5208f6
fatima <fatima13052003@gmail.com>
2024-11-18 12:59:28 -0300

    atualizando

---
+++ src/components/Oferta.jsx
@@ -0,0 +1,26 @@
+function Oferta() {
+    return (
+        <>
+            <div className="conteiner">
+                <div className="row col">
+                    <div className="col col-5 offset-1">
+                        <img src="tenis2.png" alt="" className="imgTenis" />
+                    </div>
+                    <div className="col col-6">
+                        <p>Oferta especial</p>
+                        <h2>Air Jordan edição de colecionador</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente soluta, enim voluptatibus recusandae similique repellendus repellat temporibus aliquam quaerat numquam saepe, possimus, quo ab quae illo neque praesentium amet.</p>
+                        <button>Ver oferta</button>
+                    </div>
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+
+export default Oferta
+
+
+
