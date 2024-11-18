62ca2f7b81ab159a6b5b1a9949b2fdabbd2fabad
fatima <fatima13052003@gmail.com>
2024-11-18 12:43:35 -0300

    criando minha branch

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+function Card() {
+    return (
+        <>
+            <div className="conteiner Card  d-flex">
+                <div className="row col">
+                    <div className="Card1">
+                        <h5>30% OFF</h5>
+                        <h2>Novo drop Supreme</h2>
+                        <button>Comprar</button>
+                        <img src="camisa.png" alt="" width={"250px"} />
+                    </div>
+                    <div className="Card2">
+                        <h5>30% OFF</h5>
+                        <h2>Novo drop Supreme</h2>
+                        <button>Comprar</button>
+                        <img src="tenis.png" alt="" width={"250px"} />
+                    </div>
+                    <div className="Card3">
+                        <h5>30% OFF</h5>
+                        <h2>Novo drop Supreme</h2>
+                        <button>Comprar</button>
+                        <img src="fone.png" alt="" width={"250px"} />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
