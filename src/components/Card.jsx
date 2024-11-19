d75c0bc07ad5cfc59a69b450ce0a0a61b8d33364
fatima <fatima13052003@gmail.com>
2024-11-19 11:43:01 -0300

    atualizai2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,25 +4,26 @@ import './Card.css'
 function Card() {
     return (
         <>
-            <div className="conteiner Card d-flex">
-                <div className="row col-5">
-                    <div className="Card1">
+            <div className="Conteiner Card d-flex">
+                <div className="row d-flex flex-row col-12 justify-content-sm-evenly">
+                    <h4 className="colecoes">Coleções em destaque</h4>
+                    <div className="col col-4 Card1 ">
                         <h5 className="desconto">30% OFF</h5>
                         <h2>Novo drop Supreme</h2>
                         <button className="linkComprar btn btn-light">Comprar</button>
-                        <img src="camisa.png" alt="" width={"250px"} />
+                        <img src="camisa.png" alt="" width={"250px"}/>
                     </div>
-                    <div className="Card2">
+                    <div className="col col-4 Card2">
                         <h5 className="desconto">30% OFF</h5>
-                        <h2>Novo drop Supreme</h2>
+                        <h2>Coleção Adidas</h2>
                         <button className="linkComprar btn btn-light">Comprar</button>
-                        <img src="tenis.png" alt="" width={"250px"} />
+                        <img src="tenis.png" alt="" width={"250px"}/>
                     </div>
-                    <div className="Card3">
+                    <div className="col col-4 Card3">
                         <h5 className="desconto">30% OFF</h5>
-                        <h2>Novo drop Supreme</h2>
+                        <h2>Novo Beats Bass</h2>
                         <button className="linkComprar btn btn-light">Comprar</button>
-                        <img src="fone.png" alt="" width={"250px"} />
+                        <img src="fone.png" alt="" width={"250px"}/>
                     </div>
                 </div>
             </div>
