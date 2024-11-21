c98487c21a1bb0165743e06e49f947e56700b78f
unknown <mariasilva1030@digitalcollege.internal>
2024-11-21 14:52:26 -0300

    ajustando codigo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,26 +4,26 @@ import './Card.css'
 function Card() {
     return (
         <>
-            <div className="Conteiner Card d-flex">
-                <div className="row d-flex flex-row col-12 justify-content-sm-evenly">
+            <div className="conteiner Card1 d-flex">
+                <div className="row flex-row col-12 justify-content-evenly">
                     <h4 className="colecoes">Coleções em destaque</h4>
-                    <div className="col col-4 Card1 ">
+                    <div className="col col-4 Card">
                         <h5 className="desconto">30% OFF</h5>
-                        <h2>Novo drop Supreme</h2>
+                        <h2 className="text">Novo drop Supreme</h2>
                         <button className="linkComprar btn btn-light">Comprar</button>
-                        <img src="camisa.png" alt="" width={"250px"}/>
+                        <img className="img-camisa" src="camisa.png" alt="" width={"350px"}/>
                     </div>
-                    <div className="col col-4 Card2">
+                    <div className="col col-4 Card">
                         <h5 className="desconto">30% OFF</h5>
-                        <h2>Coleção Adidas</h2>
+                        <h2 className="text">Coleção Adidas</h2>
                         <button className="linkComprar btn btn-light">Comprar</button>
-                        <img src="tenis.png" alt="" width={"250px"}/>
+                        <img className="img-tenis" src="tenis.png" alt="" width={"250px"}/>
                     </div>
-                    <div className="col col-4 Card3">
+                    <div className="col col-4 Card">
                         <h5 className="desconto">30% OFF</h5>
-                        <h2>Novo Beats Bass</h2>
+                        <h2 className="text">Novo Beats Bass</h2>
                         <button className="linkComprar btn btn-light">Comprar</button>
-                        <img src="fone.png" alt="" width={"250px"}/>
+                        <img className="img-fone" src="fone.png" alt="" width={"190px"}/>
                     </div>
                 </div>
             </div>
